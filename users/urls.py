1b0eb8bf587b84ee16d374b720d993d59f970ff2
aldgebu <aldgebu@gmail.com>
2025-06-03 03:58:05 +0400

    delete refresh token url

--- users/urls.py
+++ users/urls.py
@@ -11,8 +11,7 @@ urlpatterns = [
     # Authentication
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomTokenObtainPairView.as_view(), name='login'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
+
     # Profile management
     path('change-password/', ChangePasswordView.as_view(), name='change_password'),
     path('update-profile/', UpdateProfileView.as_view(), name='update_profile'),
